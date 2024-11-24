328839b35363c238d5c90374e037166a46c7ac0e
MarinaYoukhanva <166758761+MarinaYoukhanva@users.noreply.github.com>
2024-11-24 22:09:24 +0330

    complete CRUD methods in base repository layer

--- src/main/java/org/example/base/repository/BaseRepository.java
+++ src/main/java/org/example/base/repository/BaseRepository.java
@@ -4,8 +4,12 @@ import org.example.base.model.BaseEntity;
 
 import java.io.Serializable;
 import java.sql.SQLException;
+import java.util.Optional;
 
 public interface BaseRepository<ID extends Serializable, T extends BaseEntity<ID>> {
 
     void save(T entity) throws SQLException;
+    void update(T entity) throws SQLException;
+    void delete(ID id) throws SQLException;
+    Optional<T> findById(ID id) throws SQLException;
 }
--- src/main/java/org/example/base/repository/BaseRepositoryImpl.java
+++ src/main/java/org/example/base/repository/BaseRepositoryImpl.java
@@ -6,29 +6,80 @@ import org.example.base.model.BaseEntity;
 import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 public abstract class BaseRepositoryImpl<ID extends Serializable, T extends BaseEntity<ID>>
         implements BaseRepository<ID, T> {
-    
+
     private final Connection connection;
 
     private final String INSERT_SQL = " INSERT INTO " + " " + getTableName() + " " + getColumnName() +
             " VALUES " + getColumnCount();
 
+    private final String UPDATE_SQL = " UPDATE " + getTableName() + " SET " + getColumnsToUpdate() + " WHERE id = ? ";
+
+    private final String DELETE_SQL = " DELETE FROM " + getTableName() + " WHERE id = ? ";
+
+    private final String FIND_BY_ID_SQL = " SELECT * FROM " + getTableName() + " WHERE id = ? ";
+
     @Override
     public void save(T entity) throws SQLException {
-        PreparedStatement statement = connection.prepareStatement(INSERT_SQL);
-        fillParamsForStatement(statement, entity, false);
-        statement.execute();
-        statement.close();
+        try(PreparedStatement statement = connection.prepareStatement(INSERT_SQL, PreparedStatement.RETURN_GENERATED_KEYS)) {
+            fillParamsForStatement(statement, entity, false);
+            statement.execute();
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+                if (generatedKeys.next())
+                    entity.setId((((ID) (Serializable) generatedKeys.getInt(1))));
+            }
+        }
+    }
+
+    @Override
+    public void update(T entity) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(UPDATE_SQL)) {
+            fillParamsForStatement(statement, entity, true);
+            statement.setInt(getColumnCountNum(), (int) entity.getId());
+            statement.execute();
+        }
+    }
+
+    @Override
+    public void delete(ID id) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(DELETE_SQL)) {
+            statement.setInt(1, (int) id);
+            statement.execute();
+        }
+    }
+
+    @Override
+    public Optional<T> findById(ID id) throws SQLException {
+        try(PreparedStatement statement = connection.prepareStatement(FIND_BY_ID_SQL)) {
+            statement.setInt(1, (int) id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()){
+                T entity = getParamsFromResultSet(resultSet);
+                return Optional.of(entity);
+            }
+        }
+        return Optional.empty();
     }
 
     public abstract String getTableName();
+
     public abstract String getColumnName();
+
     public abstract String getColumnCount();
+
+    public abstract int getColumnCountNum();
+
+    public abstract String getColumnsToUpdate();
+
     public abstract void fillParamsForStatement(PreparedStatement statement,
                                                 T entity,
                                                 Boolean isCountOnly) throws SQLException;
+
+    public abstract T getParamsFromResultSet(ResultSet resultSet) throws SQLException;
 }

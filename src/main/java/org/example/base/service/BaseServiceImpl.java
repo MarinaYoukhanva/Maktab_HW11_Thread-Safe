97d23f899cc1e20b655b05ab2742a82e2c163262
MarinaYoukhanva <166758761+MarinaYoukhanva@users.noreply.github.com>
2024-11-24 22:12:53 +0330

    complete CRUD methods in base service layer

--- src/main/java/org/example/base/service/BaseServiceImpl.java
+++ src/main/java/org/example/base/service/BaseServiceImpl.java
@@ -6,6 +6,7 @@ import org.example.base.repository.BaseRepository;
 
 import java.io.Serializable;
 import java.sql.SQLException;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 public class BaseServiceImpl<ID extends Serializable, T extends BaseEntity<ID>,
@@ -15,5 +16,20 @@ public class BaseServiceImpl<ID extends Serializable, T extends BaseEntity<ID>,
     @Override
     public void save(T entity) throws SQLException {
         repository.save(entity);
+    }
+
+    @Override
+    public void update(T entity) throws SQLException {
+        repository.update(entity);
+    }
+
+    @Override
+    public void delete(ID id) throws SQLException {
+        repository.delete(id);
+    }
+
+    @Override
+    public Optional<T> findById(ID id) throws SQLException {
+        return repository.findById(id);
     }
 }
